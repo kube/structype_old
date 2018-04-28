dc2e9e425f47a5013b33eb1b753ad64cd2557dc0
Chris Feijoo <chris@feijoo.fr>
2018-04-28 19:19:36 +0200

    Format jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -9,8 +9,8 @@
       ## ## ##*/
 
 module.exports = {
-  roots: ["<rootDir>/src"],
-  testRegex: "\\.spec\\.ts$",
-  transform: { "\\.ts$": "ts-jest" },
-  moduleFileExtensions: ["ts", "js"]
-};
+  roots: ['<rootDir>/src'],
+  testRegex: '\\.spec\\.ts$',
+  transform: { '\\.ts$': 'ts-jest' },
+  moduleFileExtensions: ['ts', 'js']
+}
