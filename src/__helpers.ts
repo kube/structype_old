ff9c21389a8ed4cbe88958036738f6226355ff06
kube <chris@feijoo.fr>
2018-07-25 01:55:27 +0200

    SAVE

--- src/__helpers.ts
+++ src/__helpers.ts
@@ -10,11 +10,12 @@
 
 /**
  * Helper to check that two types are equal.
- * Both types are wrapped in an object, as unions
- * at root of type fail to pass the verification.
+ * Both types are wrapped in a tuple, as Unions
+ * at root are distributed with Conditional Types.
+ * https://github.com/Microsoft/TypeScript/issues/25912
  */
-export type IsSameStaticType<T, S> = { _: S } extends { _: T }
-  ? ({ _: T } extends { _: S } ? true : false)
+export type IsSameStaticType<T, S> = [S] extends [T]
+  ? [T] extends [S] ? true : false
   : false
 
 /**
